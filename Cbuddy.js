f62d0e6de01cbbda9e096bcbd2126f5fa11507f4
ravi <ravikpandit2003@gmail.com>
2023-11-17 23:30:28 +0530

    Check

---
+++ Cbuddy.js
@@ -0,0 +1,19 @@
+// Application Design Color 
+ 
+const ApplicationDesign = {
+    "Typography": "Arial",
+    Background: "F9F8FF",
+    NormalText: "23272A",
+    LightText: "57595A",
+    Border: "515151",
+    Button: "A020F0",
+    ButtonHover: "901DD8",
+  };
+ const menu= {
+    "login-box":{
+
+    },
+    "heroSection":{
+
+    }
+  }
---
+++ WebCbuddy.js
@@ -0,0 +1,309 @@
+// .................On Screen page...........
+
+const OnScreen ={
+    "Header": {
+        "Background": "F9F8FF",
+        "Text": "23272A"
+    },
+    "Section-1":{
+        "Background": "999FD4",
+        "Text": "FFFFFF",
+        "Button": "A020F0"
+    },
+    "Section-2":{
+        "Background": "F9F8FF",
+        "Text": "23272A"
+        
+    },
+    "Section-3":{
+        "Background": "F6F6F6",
+        "Text": "23272A"
+        
+    },
+    "Footer":{
+        "Background": "292841"
+        
+        
+    }
+    
+};
+
+//.....................Sign in page............. 
+
+const Sign_in ={
+    "Header": {
+        "Background": "F9F8FF",
+        "Text": "23272A"
+    },
+    "Section-1":{
+        "Background": "999FD4",
+        "Text": "FFFFFF",
+        "Button": "A020F0",
+
+        // ............... Card...................
+
+        "Card":{
+            "Background":"F6FBF9",
+            "Text": "23272A",
+            "Email_Button":{
+                "Background": "FFFFFF",
+                "Border": "515151",
+                "Text": "57595A"
+            },
+            "Password_Button":{
+                "Background": "FFFFFF",
+                "Border": "515151",
+                "Text": "57595A"
+            },
+            "Sign_in_Button":{
+                "Button_Color": "A020F0",
+                "Text": "FFFFFF"
+            },
+    },
+        
+    },
+    "Section-2":{
+        "Background": "F9F8FF",
+        "Text": "23272A"
+        
+    },
+    "Section-3":{
+        "Background": "F6F6F6",
+        "Text": "23272A"
+        
+    },
+    "Footer":{
+        "Background": "292841"
+        
+        
+    }
+    
+};
+
+// ............Sign-up page .....................
+
+const Sign_up ={
+    "Header": {
+        "Background": "F9F8FF",
+        "Text": "23272A"
+    },
+    "Section-1":{
+        "Background": "999FD4",
+        "Text": "FFFFFF",
+        "Button": "A020F0",
+
+        // ............... Card...................
+
+        "Card":{
+            "Background":"F6FBF9",
+            "Text": "23272A",
+            "Full_name":{
+                "Background": "FFFFFF",
+                "Border": "515151",
+                "Text": "57595A"
+            },
+            "Email_Button":{
+                "Background": "FFFFFF",
+                "Border": "515151",
+                "Text": "57595A"
+            },
+            "Password_Button":{
+                "Background": "FFFFFF",
+                "Border": "515151",
+                "Text": "57595A"
+            },
+            "Conform_Password_Button":{
+                "Background": "FFFFFF",
+                "Border": "515151",
+                "Text": "57595A"
+            },
+            "Sign_in_Button":{
+                "Button_Color": "A020F0",
+                "Text": "FFFFFF"
+            },
+    },
+        
+    },
+    "Section-2":{
+        "Background": "F9F8FF",
+        "Text": "23272A"
+        
+    },
+    "Section-3":{
+        "Background": "F6F6F6",
+        "Text": "23272A"
+        
+    },
+    "Footer":{
+        "Background": "292841"
+        
+        
+    }
+    
+};
+
+// ...............Main page ...........................
+
+const Main = {
+    "Header": {
+        "Background": "F9F8FF",
+        "Text": "23272A"
+    },
+    "Navigation":{
+
+        // .............Home Button.............
+
+        "Home_Butoon":{
+            "Background": "FFFFFF",
+            "Border": "E9EBF8",
+            "Text": "57595A",
+            "Icon": "57595A",
+
+            // ..............Hover Butoon.......
+            
+            "Hover_Butoon":{
+                "Backgorund": "E6E6E6",
+                "Border": "E9EBF8",
+                "Text": "23272A",
+                "Icon": "23272A"
+
+            }
+
+        },
+
+        // ............Study Zone Button ...............
+
+        "Study_Zone_Button":{
+            "Background": "FFFFFF",
+            "Border": "E9EBF8",
+            "Text": "57595A",
+            "Icon": "57595A",
+
+            // ..............Hover Butoon.......
+            
+            "Study_Zone_Hover_Butoon":{
+                "Backgorund": "E6E6E6",
+                "Border": "E9EBF8",
+                "Text": "23272A",
+                "Icon": "23272A"
+
+            }
+
+        },
+
+        // ................Classroom Button............
+        
+        "Classroom_Button":{
+            "Background": "FFFFFF",
+            "Border": "E9EBF8",
+            "Text": "57595A",
+            "Icon": "57595A",
+
+            // ..............Hover Butoon.......
+            
+            "Classroom_Hover_Butoon":{
+                "Backgorund": "E6E6E6",
+                "Border": "E9EBF8",
+                "Text": "23272A",
+                "Icon": "23272A"
+
+            }
+
+        },
+
+        // .........To do list Butoon..........
+
+        "To_Do_List_Button":{
+            "Background": "FFFFFF",
+            "Border": "E9EBF8",
+            "Text": "57595A",
+            "Icon": "57595A",
+
+            // ..............Hover Butoon.......
+            
+            "To_Do_List_Hover_Butoon":{
+                "Backgorund": "E6E6E6",
+                "Border": "E9EBF8",
+                "Text": "23272A",
+                "Icon": "23272A"
+
+            }
+
+        },
+
+        // ............Profile Button ............
+
+        "Profile_Button":{
+            "Background": "FFFFFF",
+            "Border": "E9EBF8",
+            "Text": "57595A",
+            "Icon": "57595A",
+
+            // ..............Hover Butoon.......
+            
+            "Profile_Hover_Butoon":{
+                "Backgorund": "E6E6E6",
+                "Border": "E9EBF8",
+                "Text": "23272A",
+                "Icon": "23272A"
+
+            }
+
+        },
+
+    },
+
+    // ......... Aside................
+
+    "Aside": {
+        
+        // ..........Notification Button ...........
+
+        "NOtification_Button": {
+            "Background": "FFFFFF",
+            "Border": "E9EBF8",
+            "Text": "57595A",
+            "Icon": "57595A",
+        },
+
+        // .............Hover...........
+
+        "Notification_Hover_Button": {
+            "Backgorund": "E6E6E6",
+            "Border": "E9EBF8",
+            "Text": "23272A",
+            "Icon": "23272A",
+        },
+
+        // .........Creat Group Button ................
+
+        "Creat_Group_Button": {
+            "Background": "FFFFFF",
+            "Border": "E9EBF8",
+            "Text": "57595A",
+            "Icon": "57595A",
+        },
+
+        // .............Hover...........
+
+        "Creat_Group_Hover_Button": {
+            "Backgorund": "E6E6E6",
+            "Border": "E9EBF8",
+            "Text": "23272A",
+            "Icon": "23272A",
+        }
+     
+    },
+
+    // ..............Contant  ...........
+
+    "Contant": {
+        "Background": "F9F8FF",
+        "Text_Name": "EBB562",
+        "Text_date": "57595A",
+        "Post_text": "23272A",
+
+    }
+   
+     
+};
